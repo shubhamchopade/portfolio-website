0bc61181aeeac58f4372077e7caf6529d090746c
Shubham Chopade <shubhamchopade10@gmail.com>
2020-08-16 01:45:57 +0530

    Updates

---
+++ precache-manifest.a8f53005b6c340dbcd45de68abfba01b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ecb56ae6bc2952b808b761351de2ee",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "3a3410984b65b6b8d771",
+    "url": "/portfolio-website/static/css/main.fcd98d5d.chunk.css"
+  },
+  {
+    "revision": "691b05df23bd86ac33e6",
+    "url": "/portfolio-website/static/js/2.d74ffa8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-website/static/js/2.d74ffa8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3410984b65b6b8d771",
+    "url": "/portfolio-website/static/js/main.9c47acb2.chunk.js"
+  },
+  {
+    "revision": "acabf9a3efb9a042b4bd",
+    "url": "/portfolio-website/static/js/runtime-main.471368d8.js"
+  },
+  {
+    "revision": "cc1825ee098eabd0270c9302314c1ac5",
+    "url": "/portfolio-website/static/media/shubham.cc1825ee.png"
+  }
+]);
